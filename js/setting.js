2c77b41b6c508799e02967cde6eaa8aadb59b603
tmatsuoka <bump1418@gmail.com>
2016-03-13 15:22:51 +0900

    #11 ݒèæʂ̐§älocalStorage©çs¤悤C³

---
+++ js/setting.js
@@ -0,0 +1,30 @@
+$(function () {
+    // 初期表示にlocalstorageから利用するサービス情報をロードする
+    $('.settingProp').each(function () {
+        if ($(this).attr('type') == 'checkbox') {
+            if(localStorage.getItem($(this).prop('id')) == 'true') {
+                $(this).prop('checked', true);
+            }
+        } else {
+            $(this).val(localStorage.getItem($(this).prop('id')));
+        }
+    });
+    
+    //useServiceにチェックが入っているサービスだけを対象に設定やタブを表示するようコントロール
+    $('input[name=useService]:checked').each(function() {
+        //alert($(this).val());
+    });
+    
+    //設定プロパティはすべてonblur時にlocalStorageに格納する
+    $('.settingProp').each(function () {
+        if ($(this).attr('type') == 'checkbox') {
+            $(this).click(function () {
+                localStorage.setItem($(this).prop('id'), $(this).prop('checked'));
+            });
+        } else {
+            $(this).blur(function () {
+                localStorage.setItem($(this).prop('id'), $(this).val());
+            });
+        }
+    });
+});
