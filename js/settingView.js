f846636faacdd40911eca612f302f47b09361e45
TakuyaMatsuoka <bump1418@gmail.com>
2016-03-17 13:18:46 +0900

    refs #15 switch buttonをbootstrapのものに差し替え

--- js/settingView.js
+++ js/settingView.js
@@ -2,24 +2,17 @@ $(function () {
     // 初期表示にlocalstorageから利用するサービス情報をロード
     $('.settingProp').each(function () {
         if ($(this).attr('type') == 'checkbox') {
-            if(localStorage.getItem($(this).prop('id')) == 'true') {
-                $(this).prop('checked', true);
-            }
+            $(this).bootstrapSwitch('state', localStorage.getItem($(this).prop('id')) == 'true' ? true : false);
         } else {
             $(this).val(localStorage.getItem($(this).prop('id')));
         }
     });
     
-    //useServiceにチェックが入っているサービスだけを対象に設定やタブを表示するようコントロール
-    $('input[name=useService]:checked').each(function() {
-        //alert($(this).val());
-    });
-    
     //設定プロパティはすべてonblur時にlocalStorageに格納
     $('.settingProp').each(function () {
         if ($(this).attr('type') == 'checkbox') {
-            $(this).click(function () {
-                localStorage.setItem($(this).prop('id'), $(this).prop('checked'));
+            $(this).on('switchChange.bootstrapSwitch', function (event, state) {
+                localStorage.setItem($(this).prop('id'), state);
             });
         } else {
             $(this).blur(function () {
@@ -28,6 +21,7 @@ $(function () {
         }
     });
     
+    //webviewのsrcを変更
     document.getElementById('tab1_frame').setAttribute('src', localStorage.getItem('url_redmine'));
     document.getElementById('tab2_frame').setAttribute('src', localStorage.getItem('url_jenkins'));
     document.getElementById('tab3_frame').setAttribute('src', localStorage.getItem('url_sonar'));
