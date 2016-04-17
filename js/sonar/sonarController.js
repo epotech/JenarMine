fdefa5f6c856c84111e5bd86984c08df6defe1af
Sakuya Inokuma <rip.sk0207@gmail.com>
2016-04-18 00:09:45 +0900

    複数プロジェクト取得メソッド用意

--- js/sonar/sonarController.js
+++ js/sonar/sonarController.js
@@ -5,11 +5,12 @@ var sonarCtr = (function() {
     function init() {
         setProjectNameFromSetting();
         writeIssueTableHtml();
+        visualizeProjectIssue();
         // 最小化ボタン押下時の挙動
         $(".minimizeSonar").on('click', function() {
-           $(".sonar-toggle").toggle(200);
-           $("#redmine_frame").css('height', '92%');
-       });
+         $(".sonar-toggle").toggle(200);
+         $("#redmine_frame").css('height', '92%');
+     });
         // 最大化ボタン押下時
         $(".maximizeSonar").on('click', function() {
             $(".sonar-toggle").toggle(200);
@@ -37,6 +38,16 @@ var sonarCtr = (function() {
         issueList.visualize();
     }
 
+    // プロジェクトのissue数をグラフ化する。
+    function visualizeProjectIssue() {
+        var projectList = getStoragedWatchProjectList();
+        if (projectList.length == 0) {
+            $("<div>Sonar解析プロジェクトが設定されていません。</div>").appendTo(".sonar-body");
+        } else {
+            // グラフ化処理実装
+        }
+    }
+
     // プロジェクトが設定されていない場合の初期表示
     function writeInitViewHtml() {
 
@@ -48,10 +59,21 @@ var sonarCtr = (function() {
     function getStoragedSonarProjectName() {
         return localStorage.getItem('projectname_sonar');
     }
+    function getStoragedWatchProjectList() {
+        var storagedWatchProjectList = [];
+        for (var i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i).startsWith('watch_projectId')) {
+              storagedWatchProjectList.push(localStorage.getItem(localStorage.key(i)));
+          }
+      }
+      return storagedWatchProjectList;
+  }
+
     //公開フィールド、メソッドを返す
     return {
         init: init,
         getStoragedSonarUrl: getStoragedSonarUrl,
-        getStoragedSonarProjectName: getStoragedSonarProjectName
+        getStoragedSonarProjectName: getStoragedSonarProjectName,
+        getStoragedWatchProjectList: getStoragedWatchProjectList
     };
 })();
