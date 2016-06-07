f2e2836df921b9ffa022a8709020acc89f5f1495
TakuyaMatsuoka <bump1418@gmail.com>
2016-06-07 18:25:32 +0900

    プロジェクトIDの取得をメソッドとして切り出し

--- js/windowControll.js
+++ js/windowControll.js
@@ -46,15 +46,21 @@ function editedProjectName() {
     });
 }
 
+//現在アクティブなプロジェクトIDを取得
 function getCurrentProjectID() {
     return parseInt(localStorage.getItem('currentProjectID'));
+}
+
+//現在アクティブなプロジェクトIDをセット
+function setCurrentProjectID(projectID) {
+    localStorage.setItem('currentProjectID', projectID);
 }
 
 //表示するプロジェクトを変更する
 function changeProject(projectID) {
     $('#bodyDiv').css('transition-duration', '1s');
     $('#bodyDiv').css('transform', 'rotateY(' + projectID * 360 + 'deg)');
-    localStorage.setItem('currentProjectID', projectID);
+    setCurrentProjectID(projectID);
 }
 
 //表示するプロジェクトを変更する（次へ）
@@ -72,6 +78,16 @@ function prevProject() {
     if (projectID != 0) {
         projectID = projectID - 1;
         changeProject(projectID);
+    }
+}
+
+//localStorageから対象のプロジェクトIDに紐付く設定情報を取得
+function getProjectSetting(projectID) {
+    var projectSetting = localStorage.getItem('ptojectID' + projectID);
+    if (!projectSetting) {
+        return new Object();
+    } else {
+        return projectSetting;
     }
 }
 
